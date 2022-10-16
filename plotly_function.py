37948f8222ab48154ef864fbc152114a057baf85
Tim YAP <64472594+tryap1@users.noreply.github.com>
2022-10-16 16:15:55 +0200

    Create plotly_function.py

---
+++ plotly_function.py
@@ -0,0 +1,6 @@
+import plotly.express as px
+from dataiku import insights
+
+def plotly_3d(df, PurchaseHour, CustomerAge, OrderTotal):
+    fig = px.scatter_3d(df_avg_purchase, x = PurchaseHour, y = CustomerAge, z = OrderTotal)
+    insights.save_plotly("3d_plot", fig)
