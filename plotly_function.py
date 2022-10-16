845db131510278933a586673603f06dea5d83150
Tim YAP <64472594+tryap1@users.noreply.github.com>
2022-10-16 16:23:05 +0200

    Update plotly_function.py

--- plotly_function.py
+++ plotly_function.py
@@ -2,5 +2,5 @@ import plotly.express as px
 from dataiku import insights
 
 def plotly_3d(df, PurchaseHour, CustomerAge, OrderTotal):
-    fig = px.scatter_3d(df_avg_purchase, x = PurchaseHour, y = CustomerAge, z = OrderTotal)
+    fig = px.scatter_3d(df, x = PurchaseHour, y = CustomerAge, z = OrderTotal)
     insights.save_plotly("3d_plot", fig)
